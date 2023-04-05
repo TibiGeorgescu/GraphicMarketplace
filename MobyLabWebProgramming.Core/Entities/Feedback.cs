7642efcf7f6bf897d29f5c569577023a439cf0f0
Tiberiu-Andrei GEORGESCU <tiberiu.georgescu00@stud.acs.upb.ro>
2023-04-05 19:32:00 +0300

    Entities

---
+++ MobyLabWebProgramming.Core/Entities/Feedback.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MobyLabWebProgramming.Core.Entities
+{
+    public class Feedback
+    {
+        public int Id { get; set; }
+        public int Rating { get; set; }
+
+        public string SenderId { get; set; }
+        public virtual User Sender { get; set; }
+
+        public string ReceiverId { get; set; }
+        public virtual User Receiver { get; set; }
+    }
+}
---
+++ MobyLabWebProgramming.Core/Entities/Product.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MobyLabWebProgramming.Core.Entities
+{
+    public class Product : BaseEntity
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string Description { get; set; }
+        [Required]
+        public decimal Price { get; set; }
+        public string ImageUrl { get; set; }
+
+        // PRODUCT OWNER
+        public int OwnerId { get; set; }
+        public User User { get; set; }
+        
+        // PRODUCT CATEGORY
+        public int CategoryId { get; set; }
+        public Categories Category { get; set; }
+    }
+}
